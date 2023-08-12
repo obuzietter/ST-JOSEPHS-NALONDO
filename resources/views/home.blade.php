e816d3452e7d5f6c729517447ad602afbe733a01
obuzietter <obuyacalvince672@gmail.com>
2023-08-12 12:58:05 +0300

    base dir
    created the base directories for front end

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nalondo - Home</title>
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <main>
+        <section class="landing">
+            <div class="welcome">
+                <div class="welcome-text">
+                    WE WELCOME YOU TO
+                </div>
+                <div class="school-name">
+                    ST JOSEPH NALONDO BOYS HIGHSCHOOL
+                </div>
+                <div class="button">
+                    <button>LEARN MORE</button>
+                </div>
+            </div>
+            
+            <div class="boxes">
+                <div class="box">
+                    <h4>
+                        Motto
+                    </h4>
+                    <p>
+                        Learning for empowerment.
+                    </p>
+                </div>
+                <div class="box">
+                    <h4>
+                        Vision
+                    </h4>
+                    <p>
+                        To become a school of excellence in education through commitment, diligence and integrity in the country.
+                    </p>
+                </div>
+                <div class="box">
+                    <h4>
+                        Mission
+                    </h4>
+                    <p>
+                        To attain academic excellence and bring out talent in young people to help the develop mentally, physically, socially and spiritually.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="slideshow">
+            <div class="slides">
+                <div class="slide"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
