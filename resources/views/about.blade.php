8a4ce2bd7b1864237c0b33e125b334a1555ccc95
obuzietter <obuyacalvince672@gmail.com>
2023-08-14 18:09:57 +0300

    about -> values

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,41 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{url('css/about.css')}}">
+    <link rel="stylesheet" href="{{ url('css/about.css') }}">
     <title>NALONDO - ABOUT</title>
 </head>
+
 <body>
     <nav>
 
     </nav>
     <main>
         <section class="badge">
-            <div class="text">
-                <h1>
-                    WHO ARE WE?
-                </h1>
+            <video controls loop autoplay>
+                <source
+                    src="https://v4.cdnpk.net/videvo_files/video/free/video0456/large_watermarked/_import_609da32c1b31b5.83573401_FPpreview.mp4"
+                    type="">
+            </video>
+            <div class="intro">
+                <div class="text">
+                    <h1>
+                        WHO ARE WE?
+                    </h1>
+                </div>
+                <div class="image">
+                    <img src="https://img.freepik.com/free-photo/shield-with-letter-n-it_1340-36069.jpg" alt="">
+                </div>
             </div>
-            <div class="image">
-                <img src="https://img.freepik.com/free-photo/shield-with-letter-n-it_1340-36069.jpg" alt="">
-            </div>
+
         </section>
         <section class="values">
-            <div class="vision-image">
+            <div class="vision">
+                <div class="vision-image">
 
+                </div>
+                <div class="vision-text">
+                    <h1>VISION</h1>
+                    <p>To become a school of excellence in education through commitment, diligence and integrity in the
+                        country.</p>
+                </div>
             </div>
-            <div class="vision-text">
-                <h1>VISION</h1>
-                <p>To become a school of excellence in education through commitment, diligence and integrity in the country.</p>
-            </div>
-            <div class="mission-image">
+            <div class="mission">
+                <div class="mission-text">
+                    <h1>MISSION</h1>
+                    <p>To attain academic excellence and bring out talent in young people to help the develop mentally,
+                        physically, socially and spiritually.</p>
+                </div>
+                <div class="mission-image">
 
-            </div>
-            <div class="mission-text">
-                <h1>MISSION</h1>
-                <p>To attain academic excellence and bring out talent in young people to help the develop mentally, physically, socially and spiritually.</p>
+                </div>
             </div>
             <div class="motto-banner">
                 <h1>MOTTO</h1>
@@ -47,7 +63,7 @@
 
             </div>
             <div class="prayer-anthem">
-                
+
             </div>
         </section>
     </main>
@@ -55,4 +71,5 @@
 
     </footer>
 </body>
+
 </html>
