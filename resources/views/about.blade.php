07119aa5a11ed92279faac44c83ec696467abac1
obuzietter <obuyacalvince672@gmail.com>
2023-08-14 21:58:24 +0300

    about page update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -58,11 +58,57 @@
                 <p>Learning for empowerment</p>
             </div>
         </section>
+        <section class="core-values">
+            <h1>CORE VALUES</h1>
+            <ol>
+                <li>HARDWORK AND TEAMWORK</li>
+                <li>TIMELINESS</li>
+                <li>INTEGRITY AND HUMILITY</li>
+                <li>CHASTITY</li>
+                <li>PERSERVERENCE</li>
+                <li>CONTINUAL IMPROVEMENT</li>
+                <li>INNOVATION AND CREATIVITY</li> 
+            </ol>
+        </section>
         <section class="history">
-            <div class="history-alumni">
-
+            <div class="history-nav">
+                <span>
+                    History of the school
+                </span>
+                <ul id='nav-items'>
+                    <li><a href="#genesis">Genesis</a></li>
+                    <li><a href="#purpose">Purpose</a></li>
+                    <li><a href="#community">Community</a></li>
+                    <li><a href="#location">Location</a></li>
+                </ul>
             </div>
-            <div class="prayer-anthem">
+            <div class="history-items" id="history-items">
+                <div class="genesis item" id="genesis">
+                    <h1>Genesis</h1>
+                    <p>Its first infrastructure was in form of a classroom block donated by the neighboring Nalondo Primary School. An interim school committee was inaugurated in 1973 with an intake of 11 students. Enrollment increased by 20 when the only secondary school in the locality Sipala Secondary School shut down and all students were absorbed into Nalondo Secondary School. The first chairman and headmaster of the school were Messrs. George Masinde and William Wasike respectively. Others were Messrs. John Masete and Peter Makokha. The chairman was an employee of the Bungoma County Council who worked hard to ensure that the school was legally registered as Harambee Mixed Secondary School in 1973. The first TSC principle was Mr. John F Simiyu from 1985 to 1993.</p>
+                </div>
+                <div class="purpose item" id="purpose">
+                    <h1>Purpose</h1> 
+                    <p>
+                        At the time, there were only a few government supported secondary schools within the then Bungoma District i.e. Bungoma High School and Kibabii High School. The school was therefore started to meet the local demand of local primary school graduates who could not afford the high fees charged by the available secondary schools over 20 km away. The first case KJSE was in 1975 And EACE was done in 1977 with seven students. Their performance was impressive as they all passed their exams. This encouraged more students to enroll at the school.
+                    </p>
+                </div>
+                <div class="community item" id="community">
+                    <h1>Community</h1>
+                    <p>
+                        Through the Harambee Spirit, the locals mobilized resources to improve the school infrastructure. Since the school started with a classroom block donated by the primary school, the community’s first business was to raise the resources to construct one classroom as compensation to the primary school. This facilitated relocation of the primary school lower unit to allow the secondary school operate independently. In addition, the secondary school section was given eight(8) acres of land where the school is situated today.  
+                    </p>
+                    <p>
+                        The late Mr. Wanjala Bunyasi gave a major boost to the school while he served as a village elder. He mobilized and encouraged local community members to bring their children to the school. In addition, the late Assistant Chief Mr. Jotham Khaemba was equally supportive. On top of marshalling students from the community, he mobilized the community to raise funds in support of the school through harambee Spirit. Others who supported the school in its initial stages are former members of Parliament, notably Messrs. Hon. Peter. Kisuya, Joseph Muliro,  John Barasa, Munyasia, Moses Wetang’ula. And the current MP, Mr. James Lusweti. The school picked up and got recognised in the locality in the 1980s and 1990s. 
+                    </p>
+                </div>
+                <div class="location item" id="location">
+                    <h1>School Location and Environment </h1>
+                    <p>
+                        The school is the only boy school in Sirare location of Nalondo Division, Bungoma Central, Sub County and Bungoma County. The people of the area are small scale farmers and some also engage in small businesses whereas others in formal employment. The soils are good with a favourable climate, while households land ownership per family averages 2 acres. Many people suffer from inadequate resources to venture into serious economic undertakings, while the market for farm produce, mainly milk, maize, sugar cane and coffee, is unreliable as prices are generally uncompetitive and keep fluctuating.
+                    </p>
+ 
+                </div>
 
             </div>
         </section>
