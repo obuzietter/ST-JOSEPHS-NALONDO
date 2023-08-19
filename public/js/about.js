379864e819fe0d91de9f30c30ea5b88bcfc880d3
obuzietter <obuyacalvince672@gmail.com>
2023-08-19 22:01:27 +0300

    Update about.js

--- public/js/about.js
+++ public/js/about.js
@@ -17,3 +17,26 @@ navItems.forEach( (item)=>{
 })
 
 document.getElementById("up").addEventListener("click", ()=>{location.href = "about"})
+
+
+// window scroll event
+/*
+const revealElement = document.getElementById('revealElement');
+
+function checkScroll() {
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+
+  if (scrollPosition > windowHeight) {
+    revealElement.style.opacity = 1;
+  } else {
+    revealElement.style.opacity = 0;
+  }
+}
+
+// Attach the scroll event listener
+window.addEventListener('scroll', checkScroll);
+
+// Initial check in case the user starts already scrolled down
+checkScroll();
+*/
