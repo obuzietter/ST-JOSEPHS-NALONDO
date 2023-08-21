d715d6591019c38ca7278fa9653daa110e373433
obuzietter <obuyacalvince672@gmail.com>
2023-08-21 15:54:47 +0300

    reset

--- thank you.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Thank You!</h1>
-        <p>Your message has been submitted successfully. We'll get back to you soon.</p>
-    </div>
-@endsection
