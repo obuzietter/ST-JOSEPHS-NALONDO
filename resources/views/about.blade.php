7ef3f392d0147b0ccc37d3d3c62d8f8490721dc6
obuzietter <obuyacalvince672@gmail.com>
2023-09-28 02:10:26 +0300

    s

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -215,8 +215,28 @@
         </section>
     </main>
     <footer>
-
     </footer>
 </body>
 
 </html>
+<script>
+    const listItems = document.querySelectorAll(".core-value");
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+
+                    entry.target.style.transform = "translateX(0)"
+                } else {
+                    entry.target.style.transform = "translateX(80%)";
+                }
+
+            });
+        }, {
+            threshold: 0.1,
+        }
+    );
+    listItems.forEach((li) => {
+        observer.observe(li);
+    });
+</script>
