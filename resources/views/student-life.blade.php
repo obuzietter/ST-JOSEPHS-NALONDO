6af7726043e57f132ecaf7c05262f44597db3460
obuzietter <obuyacalvince672@gmail.com>
2023-10-20 19:03:09 +0300

    student life skeleton

---
+++ resources/views/student-life.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Life</title>
+    <link rel="stylesheet" href="/css/student-life.css">
+
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <section class="hero">
+            <h1>LEARNING FOR EMPOWERMENT</h1>
+        </section>
+        <section class="brief">
+            <div class="box">
+                <div class="image">
+                    <img src="/images/background.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h3>STUDENT LIFE</h3>
+                    <p>LIFE AT NALONDO
+                        GIVES EVERY STUDENT A CHANCE TO PERFORM AT THEIR BEST LEVEL.</p>
+                </div>
+            </div>
+        </section>
+        <section class="clubs">
+            <div class="heading">
+                <h2>CLUBS AND EXTRA CURRICULAR ACTIVITIES</h2>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <div class="image">
+                        <img src="/images/nalondo8.jpeg" alt="">
+                    </div>
+                    <div class="desc">
+                        <h3>Eco-Warriors Club</h3>
+                        <p>The Eco-Warriors Club is a group of passionate high school students committed to making a positive impact on the environment. We organize regular clean-up events, promote recycling and sustainable living, and collaborate with local organizations to address environmental issues in our community. Join us to learn, advocate, and take practical steps toward a greener and more sustainable future.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/images/nalondo8.jpeg" alt="">
+                    </div>
+                    <div class="desc">
+                        <h3>Tech Innovators Society</h3>
+                        <p>The Tech Innovators Society is the perfect place for tech enthusiasts and aspiring innovators. We explore cutting-edge technologies, experiment with coding, and develop innovative projects. Whether you're interested in app development, robotics, or artificial intelligence, this club provides a platform to nurture your passion for all things tech.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/images/nalondo8.jpeg" alt="">
+                    </div>
+                    <div class="desc">
+                        <h3>Debate and Model UN Club</h3>
+                        <p> If you enjoy expressing your opinions, debating global issues, or simulating international diplomacy, the Debate and Model UN Club is for you. Join us to improve your public speaking skills, engage in stimulating debates, and represent your school in Model United Nations conferences. This club is the ideal platform for future leaders and diplomats.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/images/nalondo8.jpeg" alt="">
+                    </div>
+                    <div class="desc">
+                        <h3>Music Harmonics Society
+                        </h3>
+                        <p>The Music Harmonics Society welcomes all music enthusiasts, from classical virtuosos to rock aficionados. We organize jam sessions, host music appreciation events, and provide opportunities for solo and group performances. No matter your skill level, you can share your love for music in a supportive and creative environment.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/images/nalondo8.jpeg" alt="">
+                    </div>
+                    <div class="desc">
+                        <h3>Tech Innovators Society</h3>
+                        <p>The Tech Innovators Society is the perfect place for tech enthusiasts and aspiring innovators. We explore cutting-edge technologies, experiment with coding, and develop innovative projects. Whether you're interested in app development, robotics, or artificial intelligence, this club provides a platform to nurture your passion for all things tech.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="record">
+            <div class="banner">
+                <h1>HIGHEST KCSE SCORE EVER ACHIEVED <span>10.5</span></h1>
+            </div>
+        </section>
+        <section class="student-leaders">
+            <div class="container">
+                <div class="slide-show">
+                    <div class="slide">
+                        <img src="/images/nalondo22.jpeg" alt="">
+                    </div>
+                    <div class="slide">
+
+                    </div>
+                    <div class="slide">
+
+                    </div>
+                </div>
+                <div class="details">
+                    <h2>STUDENT LEADERS</h2>
+                    <h1>LEADER'S NAME</h1>
+                    <h3>CURRENT POST</h3>
+                    <h3>CLASS</h3>
+                </div>
+            </div>
+        </section>
+        <section class="religion">
+            <div class="container">
+                <div class="text">
+                    <h1>RELIGION</h1>
+                    <p>(VERTICAL SLIDESHOW AND TEXT OR A LOOPING VIDEO)</p>
+                </div>
+                <video controls loop autoplay>
+                    <source src="" type="">
+                </video>
+            </div>
+        </section>
+        <section class="accomodation">
+            <div class="boxes">
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
