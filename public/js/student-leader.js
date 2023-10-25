38a4862a529285ffc5cffd02bcdd06daf6ac84a8
obuzietter <obuyacalvince672@gmail.com>
2023-10-25 22:07:47 +0300

    sa

--- public/js/student-leader.js
+++ public/js/student-leader.js
@@ -5,17 +5,17 @@ const text1_options = [
     "BOARDING CAPTAIN "
   ];
   const text2_options = [
-    "69 min. read",
-    "7 min. read",
-    "8 min. read",
-    "87,658.1277 min. read"
+    "FORM 4 NORTH EAST",
+    "FORM 4 WEST",
+    "FORM 4 EAST",
+    "FORM 4 SOUTH"
   ];
   const color_options = ["#EBB9D2", "#FE9968", "#7FE0EB", "#6CE5B1"];
   const image_options = [
     "/images/achieve.jpg",
-    "https://images.unsplash.com/photo-1556656793-08538906a9f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1506073828772-2f85239b6d2d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
-    "https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+    "/images/nalondo30.jpeg",
+    "/images/nalondo8.jpeg",
+    "/images/achievement3.jpeg"
   ];
   var i = 0;
   const currentOptionText1 = document.getElementById("current-option-text1");
