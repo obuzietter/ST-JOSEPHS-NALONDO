e19535093ecc59517479b091cdf361d55b98f0db
obuzietter <obuyacalvince672@gmail.com>
2023-11-10 23:15:14 +0300

    navbar on home page

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,15 +1,16 @@
 <header class="top-nav">
-    <div>Nalondo High School</div>
+    <div class="logo">Nalondo High School</div>
     <input id="menu-toggle" type="checkbox" />
     <label class="menu-button-container" for="menu-toggle">
         <div class="menu-button"></div>
     </label>
     <ul class="menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Faculty</a></li>
-        <li><a href="#">Student Life</a></li>
-        <li><a href="#">Achievements</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('about') }}">About</a></li>
+        <li><a href="{{ route('faculty') }}">Faculty</a></li>
+        <li><a href="{{ route('studentLife') }}">Student Life</a></li>
+        <li><a href="{{ route('community') }}">Community</a></li>
+        <li><a href="{{ route('achievements') }}">Achievements</a></li>
+        <li><a href="{{ route('contact') }}">Contact Us</a></li>
     </ul>
 </header>
