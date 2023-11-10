064bea96a6e48985110ac2ffa805106ef4ac75da
obuzietter <obuyacalvince672@gmail.com>
2023-11-10 22:14:26 +0300

    dynamic navbar created

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+<header class="top-nav">
+    <div>Nalondo High School</div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+    </label>
+    <ul class="menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Faculty</a></li>
+        <li><a href="#">Student Life</a></li>
+        <li><a href="#">Achievements</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</header>
