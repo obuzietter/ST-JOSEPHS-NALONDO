933e66308bbf7c2173472909cf147e19b24c2671
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2023-11-11 19:23:34 +0300

    added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/theme.css">
     <link rel="stylesheet" href="/css/navbar.css">
+    <link rel="stylesheet" href="/css/footer.css">
+
 
     <script src="/js/home.js" defer></script>
 
