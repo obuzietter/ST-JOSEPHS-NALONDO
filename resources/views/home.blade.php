6454d1c3bf76331365b5625dbe91fbe3580ebc6d
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2023-11-13 22:08:50 +0300

    changes made

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -302,7 +302,48 @@
         })
     })
 
+    window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
+})
 
+//show/hide nav menu
+const menu=document.querySelector(".nav_menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+
+
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+
+//close navbar
+const closeNav=()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+
+
+}
+closeBtn.addEventListener('click',closeNav)
+
+      var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      //when windo is>=600px
+      breakpoints: {
+        600:{
+            slidesPerView:2
+
+        }
+      }
+    });
   </script>
 
 </html>
